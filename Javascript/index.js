6bf877cdfbde68f37f8cf906a32b0c8ca41cfea5
Akin Patel <apatel20@students.kgi.edu>
2022-05-05 20:52:51 -0700

    Added blur fnct

--- Javascript/index.js
+++ Javascript/index.js
@@ -7,3 +7,9 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+/*Blurring fncts*/
+function unblurImage() {
+  let root = document.querySelector(':root')
+  root.style.setProperty('--blur', '0px')
+  root.style.setProperty('--element-display', 'none')
+}
