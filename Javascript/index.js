9545329ae399e6ec49cf8089634d8e6b3e6ea283
Akin Patel <apatel20@students.kgi.edu>
2022-05-11 20:29:45 -0700

    Merge branch 'main' of https://github.com/apatel1026/apatel1026.github.io

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,6 +1,8 @@
 /* Unblurring functionality  */
 function unblurImage() {
+  /*query slector spreading blur fnct across all posts, would need remake for new object array*/
   let root = document.querySelector(':root')
+  /*on-click un blurring */
   root.style.setProperty('--blur', '0px')
   root.style.setProperty('--element-display', 'none')
 }
